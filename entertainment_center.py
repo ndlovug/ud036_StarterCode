abab1098cf1fb1ee3f4423aa33c40a4d8d87c3f2
Greatjoy Ndlovu <ndlovug@users.noreply.github.com>
2017-07-24 13:39:24 -0700

    Add movie posters

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,52 +2,42 @@ import fresh_tomatoes
 import media
 
 toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that come to life",
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
-print(toy_story.storyline)
-
 avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar_Teaser-Poster.jpg",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=-9ceBgWV8io")
 
-print(avatar.storyline)
-
 good_will_hunting = media.Movie("Good Will Hunting",
-                                "A story about a gifted but troubled young man and an MIT professor "
-                                "who decides to help the misguided youth reach his potential",
-                                "https://en.wikipedia.org/wiki/Good_Will_Hunting#/media/File:"
-                                "Good_Will_Hunting_theatrical_poster.jpg",
+                                "https://upload.wikimedia.org/wikipedia/en/b/b8/Good_Will_Hunting_theatrical_poster.jpg",
                                 "https://www.youtube.com/watch?v=PaZVjZEFkRs")
 
-print(good_will_hunting.storyline)
-
 hitch = media.Movie("Hitch",
-                    "A movie about a 'date doctor' who coaches other men in the art of wooing women, "
-                    "with a focus on long-term relationships.",
-                    "https://en.wikipedia.org/wiki/Hitch_(film)#/media/File:Hitch_poster.JPG",
+                    "https://upload.wikimedia.org/wikipedia/en/d/d4/Hitch_poster.JPG",
                     "https://www.youtube.com/watch?v=dMaq_pfxs-0")
 
 school_of_rock = media.Movie("School of Rock",
-                             "Using rock music to learn",
                              "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
                              "https://www.youtube.com/watch?v=3PsUJFEBC74")
 
 ratatouille = media.Movie("Ratatouille",
-                          "A rat is a chef in Paris",
-                          "http://upload.wikimedia.org/wikipedia/5/50/RatatouillePoster.jpg",
+                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
                           "https://www.youtube.com/watch?v=c3sBBRxDAqk")
 
 midnight_in_paris = media.Movie("Midnight in Paris",
-                                "Going back in time to meet authors",
-                                "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_poster.jpg",
+                                "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
                                 "https://www.youtube.com/watch?v=atLg2wQQxvU")
 
 hunger_games = media.Movie("Hunger Games",
-                           "A really real reality show",
                            "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
                            "https://www.youtube.com/watch?v=PbA637H0bo")
 
-movies = [toy_story, avatar, good_will_hunting, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+the_martian = media.Movie("The Martian",
+                           "https://upload.wikimedia.org/wikipedia/en/c/cd/The_Martian_film_poster.jpg",
+                           "https://www.youtube.com/watch?v=Ue4PCI0NamI")
+
+movies = [toy_story, avatar, good_will_hunting, hitch, school_of_rock, ratatouille, midnight_in_paris, hunger_games,
+          the_martian]
+
+fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -3,8 +3,7 @@ class Movie(object):
 
     # VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
-        self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
